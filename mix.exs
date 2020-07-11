046914eb8ae431caed28e544b0bbd1f38ffacc09
Jonathan Arnett <jonarnett90@gmail.com>
2020-07-11 00:36:59 -0400

    Add additional metadata to mix.exs

--- mix.exs
+++ mix.exs
@@ -1,15 +1,23 @@
 defmodule Meseeks.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/j3rn/meseeks"
+
   def project do
     [
       app: :meseeks,
       name: "Meseeks",
+      description: "Can do! A simple Elixir authorization system!",
       version: "0.1.0",
+      source_url: @source_url,
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      package: [
+        licenses: ["MIT"],
+        links: %{source: @source_url}
+      ],
       docs: [
         main: "Meseeks"
       ]
